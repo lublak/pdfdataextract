9468dc2584190a68fe1c87c54c2d5fbc055b95d8
lublak <lublak.de@gmail.com>
2021-04-15 16:06:34 +0200

    add first test

---
+++ test/basic.test.ts
@@ -0,0 +1,21 @@
+const PDF_TEST_FILE = './test/basic.pdf';
+
+import { PdfData } from '../src/pdfdata';
+import { readFileSync} from 'fs';
+
+describe(`parse ${PDF_TEST_FILE}`, () => {
+    const data = readFileSync(PDF_TEST_FILE);
+    it('without password should fail', async () => {
+      await expect(PdfData.parse(data)).rejects.toThrow();
+    });
+    it('extract basic data', async () => {
+      const result = await PdfData.parse(data, {
+        password: '123456'
+      });
+      expect(result.pages).toEqual(2);
+      expect(result.text.length).toEqual(2);
+      const first_page_lines = result.text[0].split('\n');
+      expect(first_page_lines.length).toEqual(35);
+      expect(first_page_lines[10]).toMatch(/^dapibus mattis/);
+    });
+});
